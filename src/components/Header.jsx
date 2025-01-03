999e78071f9dd07f368abc4ed6e0e75ee7c50ca3
Shaiful <shaiful48027@gmail.com>
2025-01-03 13:11:07 +0600

    Setup Website

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import moment from "moment";
+import logo from "../assets/logo.png"
+
+const Header = () => {
+    return (
+        <div>
+            <div className="logo my-6 max-w-md mx-auto text-center">
+                <img src={logo} alt="#" />
+                <h2 className="text-lg my-2">Journalism Without Fear or Favour</h2>
+                <p className="text-black font-medium my-2">{moment().format("dddd, MMMM Do YYYY")}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
+
+
+
---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,14 @@
+
+
+const LatestNews = () => {
+    return (
+        <div className="bg-[#F3F3F3] p-4 mx-auto">
+            <div className="flex gap-1 items-center">
+                <p className="p-2 bg-[#D72050] text-white">Latest</p>
+                <p className="p-2 text-lg text-[#403F3F] font-semibold">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+            </div>
+        </div>
+    );
+};
+
+export default LatestNews;
